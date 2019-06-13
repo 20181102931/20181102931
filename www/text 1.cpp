6e183f0aa0c03b705d6b1085fa09cddd5c5eff8a
20181102931 <18647197975@163.com>
2019-06-13 12:00:15 +0800

    1234
    444

---
+++ www/text 1.cpp
@@ -0,0 +1,9 @@
+//
+//  text 1.cpp
+//  www
+//
+//  Created by s20181102931 on 2019/6/13.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include "text 1.hpp"
---
+++ www/text 1.hpp
@@ -0,0 +1,14 @@
+//
+//  text 1.hpp
+//  www
+//
+//  Created by s20181102931 on 2019/6/13.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#ifndef text_1_hpp
+#define text_1_hpp
+
+#include <stdio.h>
+
+#endif /* text_1_hpp */
